384dcaeeb560498bd9eaf5e3a6ab56667323fa9e
zhao rui <rui.zhao@shopeemobile.com>
2018-01-22 22:53:35 +0800

    updated

--- code/main_test.py
+++ code/main_test.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 from numpy.random import seed
@@ -47,27 +47,27 @@ if __name__ == '__main__':
 		score= mean_squared_error(y_pred, y_test)
 		mae_score = mean_absolute_error(y_pred, y_test)
 		logging.info("   randomforest  result: %f_%f" %(score, mae_score))
+		#neural network
 		nn_model = build_NN(data_dim)
 		nn_model.fit(X_train, y_train, epochs=NUM_BPEPOCH, batch_size=BATH_SIZE)
 		y_pred = nn_model.predict(X_test)
 		score= mean_squared_error(y_pred, y_test)
 		mae_score = mean_absolute_error(y_pred, y_test)
 		logging.info("   neural network result: %f_%f" %(score, mae_score))
-
+		#AE
 		normal_AE = build_pre_normalAE(data_dim, X_train, epoch_pretrain=NUM_PREEPOCH, hidDim=[140,280])
 		normal_AE.fit(X_train, y_train, epochs=NUM_BPEPOCH, batch_size=BATH_SIZE)
 		y_pred = normal_AE.predict(X_test)
 		score = mean_squared_error(y_pred, y_test)
 		mae_score = mean_absolute_error(y_pred, y_test)
 		logging.info("   normal ae result: %f_%f" %(score, mae_score))
-
+		#denoise AE
 		denois_AE = build_pre_denoiseAE(data_dim, X_train, epoch_pretrain=NUM_PREEPOCH, hidDim=[140,280])
 		denois_AE.fit(X_train, y_train, epochs=NUM_BPEPOCH, batch_size=BATH_SIZE)
 		y_pred = denois_AE.predict(X_test)
 		score = mean_squared_error(y_pred, y_test)
 		mae_score = mean_absolute_error(y_pred, y_test)
 		logging.info("   denoise ae result: %f_%f" %(score, mae_score))
-
 		# rbf using sigmoid function, feature should be scaled to -1 and 1
 		scaler = MinMaxScaler()
 		X_train_rbm = scaler.fit_transform(X_train)
@@ -77,8 +77,8 @@ if __name__ == '__main__':
 		y_pred = rbm.predict(X_test_rbm)
 		score = mean_squared_error(y_pred, y_test)
 		mae_score = mean_absolute_error(y_pred, y_test)
-		logging.info("   rbm result: %f_%f" %(score, mae_score))
-
+		logging.info("   dbn result: %f_%f" %(score, mae_score))
+		#Bi-directional LSTM
 		t = load_data(False)
 		X_train, y_train, X_test, y_test = t[0], t[1], t[2], t[3]
 		data_dim = X_train.shape[2]
@@ -89,14 +89,14 @@ if __name__ == '__main__':
 		score = mean_squared_error(y_pred, y_test)
 		mae_score = mean_absolute_error(y_pred, y_test)
 		logging.info("   bi-lstm result: %f_%f" %(score, mae_score))
-
+		#LSTM
 		LSTM = build_LSTM(timesteps, data_dim)
 		LSTM.fit(X_train, y_train, epochs=NUM_BPEPOCH, batch_size=BATH_SIZE)
 		y_pred = LSTM.predict(X_test)
 		score = mean_squared_error(y_pred, y_test)
 		mae_score = mean_absolute_error(y_pred, y_test)
 		logging.info("   lstm result: %f_%f" %(score, mae_score))
- 
+		#CNN
 		CNN = build_CNN(timesteps, data_dim)
 		CNN.fit(X_train, y_train, epochs=NUM_BPEPOCH, batch_size=BATH_SIZE)
 		y_pred = CNN.predict(X_test)
