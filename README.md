3a15b60889e20a0f3c56d171ed97b5edb9a52c1d
zhao rui <rui.zhao@shopeemobile.com>
2018-05-28 23:51:22 +0800

    updated for mhms

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Deep Learning and Its Applications to Machine Health Monitoring
 
+This module contain the code used in our survey paper: 'Deep Learning and Its Applications to Machine Health Monitoring'.
+
 ## Table of Contents
 
 <!-- TOC START min:1 max:3 link:true update:true -->
 - [Deep Learning and Its Applications to Machine Health Monitoring: A Survey](#DL-MHMS)
   - [Table of Contents](#table-of-contents)
   - [Data](#data)
-  - [Code](#recently-view)
+  - [Code](#code)
     - [Feature Extraction](#feature-extraction)
-    - [Deep Learning Models](#DL-models)
-    - [Machine Learning Models](#ml-models)
-    - [Main Test](#maintest)
+    - [Deep Learning Models](#deep-learning-models)
+    - [Machine Learning Models](#machine-learning-models)
+    - [Main Test](#main-test)
 <!-- TOC END -->
 
 
@@ -21,22 +23,21 @@ This folder contains two pickle files, which are extracted features and labels f
 - data_normal: each data sample is a vector. The features are extracted from the whole time sequences. 
 - data_seq: each data sample is a tensor. The features are extracted from windows of the time time sequences. 
 
-Especially, data_seq can be used by LSTM and CNN models. ß
+Especially, data_seq can be used by LSTM and CNN models. data_normal can be utilized by conventional ML models.
 
 ## Code
-This folder contains codes for feature extractions, traditional machine learning models, deep learning models and test modules. 
+This folder contains codes for feature extraction, traditional machine learning models, deep learning models and test modules. 
 
 ### Feature Extraction
 RMS, VAR, MAX, Peak, Skew, Kurt, Wavelet, Spectral Kurt, Spectral Skewness, Spectral Powder features are extracted from the input time series. 
 
 ### Deep Learning Models
-Based on Keras, autoencoder and its variants, DBN, LSTM, Bi-directional LSTM and CNN models are provided
+Based on Keras, autoencoder and its variants, implementations of DBN, LSTM, Bi-directional LSTM and CNN models are provided
 
 ### Traditioanl Machine Learning Models
 SVR with two kernels (linear and rbf), Random Forest and Neural Network are provided.
 
 ### Main Test
-
 To replicate the results reported in paper (python 2.7)
 ```
 pip install -r requirement 
--- code/feature_extract.py
+++ code/feature_extract.py
@@ -1,6 +1,6 @@
-
-
-
+"""
+This script define various feature extraction methods
+"""
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -15,7 +15,6 @@ from numpy import mean, sqrt, square
 import scipy.stats as sts
 from subprocess import Popen, PIPE
 from pywt import WaveletPacket
-
 
 
 def rms_fea(a):
@@ -60,7 +59,7 @@ def spectral_pow(a):
 	return np.mean(np.power(mag, 3))
 
 
-def extract_fea(data, num_stat =5):
+def extract_fea(data, num_stat = 10):
 	# input: time_len * dim_fea  -> dim_fea*9
 	data_fea = []
 	dim_feature = 1
@@ -68,18 +67,24 @@ def extract_fea(data, num_stat =5):
 		data_slice = data
 		data_fea.append(rms_fea(data_slice))
 		data_fea.append(var_fea(data_slice))
-		#data_fea.append(max_fea(data_slice))
-		#data_fea.append(pp_fea(data_slice))
-		#data_fea.append(skew_fea(data_slice))
+		data_fea.append(max_fea(data_slice))
+		data_fea.append(pp_fea(data_slice))
+		data_fea.append(skew_fea(data_slice))
 		data_fea.append(kurt_fea(data_slice))
-		#data_fea.append(wave_fea(data_slice))
-		data_fea.append(spectral_skw(data_slice))
+		data_fea.append(wave_fea(data_slice))
 		data_fea.append(spectral_kurt(data_slice))
-		#data_fea.append(spectral_pow(data_slice))		
+		data_fea.append(spectral_skw(data_slice))
+		data_fea.append(spectral_pow(data_slice))
 	data_fea = np.array(data_fea)
 	return data_fea.reshape((1,dim_feature*num_stat))
 		
-def gen_fea(data,time_steps=20,num_stat = 5):
+def gen_fea(data,time_steps = 20,num_stat = 10):
+	"""
+	input: 
+		@data: raw time series data, [data size,  sequence length]
+		@time_steps: the number of windows, and it can be one
+		@num_stat: number of features for each window
+	"""
 	data_num = data.shape[0]
 	len_seq = data.shape[1]
 	window_len = len_seq/time_steps
@@ -96,36 +101,3 @@ def gen_fea(data,time_steps=20,num_stat = 5):
 				temp_data = extract_fea(sig_data[start:end])
 				new_data[idxdata,i,:] = temp_data
 	return new_data
-
-
-
-def short_data(data_x, data_y):
-	new_len = 1000
-	data_new_x = np.zeros((data_x.shape[0]*4, new_len))
-	data_new_y = np.zeros((data_x.shape[0]*4,))
-	for ith in range(data_x.shape[0]):
-		data_new_x[ith*4] =  data_x[ith,0:new_len]
-		data_new_y[ith*4] = data_y[ith]  
-		data_new_x[ith*4+1] = data_x[ith,new_len:2*new_len]
-		data_new_y[ith*4+1] = data_y[ith]  
-		data_new_x[ith*4+2] = data_x[ith,2*new_len:3*new_len]
-		data_new_y[ith*4+2] = data_y[ith] 
-		data_new_x[ith*4+3] = data_x[ith,3*new_len:4*new_len]
-		data_new_y[ith*4+3] = data_y[ith]  
-	return data_new_x, data_new_y
-
-
-def main():
-	
-	x = cPickle.load(open("../data/cwru.p","rb"))
-	data_x, data_y = x[0], x[1]
-	print 'data loaded'
-	new_data_x, new_data_y = short_data(data_x, data_y)
-	data_time = gen_fea(new_data_x)
-	data_normal = gen_fea(new_data_x, 1)
-	len_data = data_normal.shape[0]
-	cv_idx = np.random.randint(3, size=len_data)
-	cPickle.dump([new_data_x, data_time, data_normal, new_data_y, cv_idx], open('../data/cwrupuredata.p', "wb"))
-
-if __name__ == '__main__':
-	main()
